a8fd2d2efb3008a6d26ae4ab5e47b9c339bc5630
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-26 20:31:15 +0530

    fixing layout

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -175,7 +175,7 @@ const Ecommerce = () => {
             </div>
           </div>
 
-          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-[345px] p-8 m-3 flex justify-center items-center gap-10">
             <div>
               <p className="text-2xl font-semibold ">$43,246</p>
               <p className="text-gray-400">Yearly sales</p>
